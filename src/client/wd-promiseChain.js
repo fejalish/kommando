2ed4812f4958f19e74c29d15ab50f672b943191f
Jeff Dawson <jeff@maqe.com>
2015-09-29 10:49:37 +0700

    switching log4js to winston

--- src/client/wd-promiseChain.js
+++ src/client/wd-promiseChain.js
@@ -6,49 +6,54 @@ var url = require('url');
 var wd = require('wd');
 
 var colors = require('colors');
-// var log4js = require('log4js');
-// log4js.loadAppender('file');
-// log4js.addAppender(log4js.appenders.file('./test-results/results.log'), 'results');
-// var logger = log4js.getLogger('results');
+//create logger and save all output to log file
+//don't show in console since it can be info overload
+//TODO:
+//  include debug option...
+//
+var winston = require('winston');
+winston.add(winston.transports.File, { filename: './logs/webdriver.log' });
+winston.remove(winston.transports.Console);
+
 
 module.exports = function(seleniumUrl) {
   return {
-    clients: {},
-    create: function(capabilities, callback) {
-      var client = wd.promiseChainRemote(url.parse(seleniumUrl));
+	clients: {},
+	create: function(capabilities, callback) {
+	  var client = wd.promiseChainRemote(url.parse(seleniumUrl));
 
-      client.on('status', function(info) {
-        // logger.info(info);
-        console.log(info.cyan);
-      });
-      client.on('command', function(eventType, command, response) {
-        // logger.info(eventType, command, (response || ''));
-        console.log(' > ' + eventType.cyan, command, (response || '').grey);
-      });
-      client.on('http', function(meth, path, data) {
-        // logger.info(meth, path, (data || ''));
-        console.log(' > ' + meth.magenta, path, (data || '').grey);
-      });
+	  client.on('status', function(info) {
+		winston.info(info);
+		// console.log(info.cyan);
+	  });
+	  client.on('command', function(eventType, command, response) {
+		winston.info(eventType, command, (response || ''));
+		// console.log(' > ' + eventType.cyan, command, (response || '').grey);
+	  });
+	  client.on('http', function(meth, path, data) {
+		winston.info(meth, path, (data || ''));
+		// console.log(' > ' + meth.magenta, path, (data || '').grey);
+	  });
 
-      client.init(capabilities, function(error, sessionId) {
-        this.clients[sessionId] = client;
-        if (typeof callback === 'function') {
-          callback(error, client, {
-            wd: wd
-          });
-        }
-      }.bind(this));
-    },
-    end: function(callback) {
-      var client;
-      var quitFunctions = [];
+	  client.init(capabilities, function(error, sessionId) {
+		this.clients[sessionId] = client;
+		if (typeof callback === 'function') {
+		  callback(error, client, {
+			wd: wd
+		  });
+		}
+	  }.bind(this));
+	},
+	end: function(callback) {
+	  var client;
+	  var quitFunctions = [];
 
-      for (var id in this.clients) {
-        client = this.clients[id];
-        delete this.clients[id];
-        quitFunctions.push(client.quit.bind(client));
-      }
-      async.series(quitFunctions, callback);
-    }
+	  for (var id in this.clients) {
+		client = this.clients[id];
+		delete this.clients[id];
+		quitFunctions.push(client.quit.bind(client));
+	  }
+	  async.series(quitFunctions, callback);
+	}
   };
 };
